43996e9a4c086b8106cf3cbaff556595944a3bab
Barry Shovlin <bshovlin32@gmail.com>
2021-03-02 16:04:46 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+Main();
+
+void Main()
+{
+    string question = "empty";
+    Random random = new Random();
+
+    while (question != "")
+    {
+        Console.WriteLine("Ask away, child");
+        question = Console.ReadLine();
+
+        if (question == "")
+        {
+            Console.WriteLine("You missed your chance");
+            break;
+        }
+
+
+    }
+}
